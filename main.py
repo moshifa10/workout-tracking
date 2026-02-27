e691d235e848ef85c5f25abaaa1856c13001a81f
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-02-28 01:50:59 +0200

    done with the projects

--- main.py
+++ main.py
@@ -1,3 +1,63 @@
+import requests
+import dotenv
+import os
+import pprint
+import datetime as dt
+
+dotenv.load_dotenv()
+ENDPOINT = "https://app.100daysofpython.dev"
+
+excercise = "/v1/nutrition/natural/exercise"
+
+api_key = os.getenv(key="API_KEY")
+app_id = os.getenv(key="APP_ID")
+api_post = os.getenv(key="API_POST")
+api_get = os.getenv(key="API_GET")
+params = {
+    # "Content-Type": "application/json",
+    "x-app-id": app_id,
+    "x-app-key": api_key
+}
+
+user = input("Tell me which excersise you did? ")
+
+def call(body):
+    response = requests.post(url=ENDPOINT+excercise, headers=params, json=body)
+    response.raise_for_status()
+    result = response.json()
+    # print(pprint.pprint(result))
 
 
-API Key Created! Save this key - it will only be shown once Important: Copy and save this API key now. For security reasons, you won't be able to see it again. API Key nix_live_GgVBCFp5WJMpeoBOKQYUQsWxUSp9NqJx App ID app_4b6c65e46fbf49bbbd1290ad Use these credentials in your API requests by including them as headers: x-app-id: app_4b6c65e46fbf49bbbd1290ad x-app-key: nix_live_GgVBCFp5WJMpeoBOKQYUQsWxUSp9NqJx
+    now = dt.datetime.now()
+
+    shetty = requests.get(url=api_get)
+    shetty.raise_for_status()
+    # print(shetty.text)
+
+    data = shetty.json()
+    # print(data)
+    info = {
+        "date": now.strftime("%d/%m/%Y"),
+        "time": now.strftime("%H:%M:%S"),
+        "exercise": result["exercises"][0]["name"],
+        "duration": result["exercises"][0]["duration_min"],
+        "calories": result["exercises"][0]["nf_calories"],
+        # "id": data["workouts"][-1]["id"] +1
+    }
+    # data["workouts"].append(info)
+    shitty_body = {
+        "workout": info
+    }
+    # print(data)
+    shitty_post  = requests.post(url=api_post, json=shitty_body)
+
+    # print(shitty_post.text)
+    shitty_post.raise_for_status()
+# print(now)
+if "." in user:
+    for i in user.split("."):
+        body = {"query":  f"{i}"}
+        call(body)
+else:
+    body = {"query":  f"{user}"}
+    call(body)
